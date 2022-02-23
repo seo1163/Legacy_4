3b5429a662836c67cd31c87acef4262b4f18f5a4
seo1163 <seo1163@naver.com>
2022-02-23 17:47:05 +0900

    bankbook1

---
+++ src/main/java/com/iu/s1/bankbook/BankBookDAO.java
@@ -0,0 +1,41 @@
+package com.iu.s1.bankbook;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BankBookDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.iu.s1.bankbook.BankBookDAO.";
+
+	//update
+	public int update(BankBookDTO bankBookDTO)throws Exception {
+		return sqlSession.update(NAMESPACE+"update", bankBookDTO);
+	}
+	
+	//detail
+	public BankBookDTO detail(BankBookDTO bankBookDTO)throws Exception{
+		return sqlSession.selectOne(NAMESPACE+"detail", bankBookDTO);
+	}
+
+	//list
+	public List<BankBookDTO> list() throws Exception{
+		return sqlSession.selectList(NAMESPACE+"list");
+	}
+	
+	//insert
+	public int add(BankBookDTO bankBookDTO)throws Exception{
+		return sqlSession.insert(NAMESPACE+"add", bankBookDTO);
+	}
+	
+	//delete
+	public int delete(BankBookDTO bankBookDTO)throws Exception{
+		return sqlSession.delete(NAMESPACE+"delete", bankBookDTO);
+	}
+	
+}
